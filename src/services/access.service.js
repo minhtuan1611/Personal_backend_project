231be3b32cabd92258aba58c2da9a3afb6347c9b
minhtuan1611 <tuan.mn51@gmail.com>
2023-11-24 23:00:06 +0200

    JWT_with_RSA

--- src/services/access.service.js
+++ src/services/access.service.js
@@ -36,38 +36,26 @@ class AccessService {
       })
 
       if (newShop) {
-        // created privateKey, publicKey
-        const { privateKey, publicKey } = crypto.generateKeyPairSync('rsa', {
-          modulusLength: 4096,
-          publicKeyEncoding: {
-            type: 'pkcs1',
-            format: 'pem',
-          },
-          privateKeyEncoding: {
-            type: 'pkcs1',
-            format: 'pem',
-          },
-        })
+        const privateKey = crypto.randomBytes(64).toString('hex')
+        const publicKey = crypto.randomBytes(64).toString('hex')
 
         console.log({ privateKey, publicKey })
 
-        const publicKeyString = await KeyTokenService.createKeyToken({
+        const keyStore = await KeyTokenService.createKeyToken({
           userId: newShop._id,
           publicKey,
+          privateKey,
         })
-        if (!publicKeyString) {
+        if (!keyStore) {
           return {
             code: 'xxxx',
-            message: 'PublicKeyString error',
+            message: 'KeyStore error',
           }
         }
-        console.log(`PublicKeyString:`, publicKeyString)
-
-        const publicKeyObject = crypto.createPublicKey(publicKeyString)
 
         const tokens = await createTokenPair(
           { userId: newShop._id, email },
-          publicKeyString,
+          publicKey,
           privateKey
         )
         console.log(`Created Token Success: `, tokens)
